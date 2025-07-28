5d6df862e921760d5966226ea83bbf571ec53b4e
Abderrahmane Boukhelif <boukhelifeabderrahman@etu.univ-blida.dz>
2025-07-28 13:01:37 +0100

    api

---
+++ src/services/api.js
@@ -0,0 +1,16 @@
+const API_KEY = "196983dbf67d4aca6c3471f81e031484"
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () => {
+    const response = await fetch('${BASE_URL}/movie/popular?api_key=${API_KEY}')
+    const data = response.json()
+    return data.results
+
+}
+
+export const searchMovies = async () => {
+    const response = await fetch('${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}')
+    const data = response.json()
+    return data.results
+
+}
